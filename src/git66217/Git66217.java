c03413973fd68d71d27dd4c2f94437ceb280b21d
lampi <lampi@192.168.1.70>
2021-02-18 20:20:12 -0600

    Estoy igualando mis dos cipoas el working copy y el local repository.

---
+++ src/git66217/Git66217.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git66217;
+
+/**
+ *
+ * @author lampi
+ */
+public class Git66217 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Otniel Martinez";
+        String matricula = "66217";
+        
+        System.out.println("Soy " + nombre + " y mi matricula es " + matricula);
+        //Acavo de crear mi respositorio local en team  
+        
+        int edad= 22;
+    }
+    
+}
